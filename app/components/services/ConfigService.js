66e2903e92423cbeab45943c1ef22fddaa8f83c9
Haythem Sahbani <EXTERN.SAHBANI_HAYTHEM@ALLIANZ.DE>
2016-07-07 11:52:59 +0200

    first rest test

--- app/components/services/ConfigService.js
+++ app/components/services/ConfigService.js
@@ -4,13 +4,15 @@ angular.module('learnzillaApp')
   .service('ConfigService', ['$resource', '$location', function ($resource, $location) {
     const service = this;
 
-    service.protocol = 'http';
+/*    service.protocol = 'http';
     service.url = $location.hostname;
     service.port = '8080';
-    service.baseWsUrl = `${service.protocol}://${service.url}:${service.port}`;
+    service.baseWsUrl = `${service.protocol}://${service.url}:${service.port}`;*/
 
 
-    service.login = $resource(url, '[paramDefaults]', '[actions]', 'options');
+    service.login = $resource('localhost:8080/login',  {user: "@user", password: "@password"});
+    service.logout = $resource('localhost:8080/logout?benutzerId=123');
+    
 
 
     return service;
--- app/components/services/UserService.js
+++ app/components/services/UserService.js
@@ -1,17 +1,27 @@
 'use strict'
 
 angular.module('learnzillaApp')
-  .service('UserService', ['$resource', function ($resource) {
+  .service('UserService', ['ConfigService', function (ConfigService) {
     const service = this;
-$resource.getRe
+
     const user = {
-      id: null,
-      name: '',
-      psudoname: '',
-      highScore: 0,
-      email: ''
+      benutzerId: null,
+      benutzerName: 'Gast',
+      fettigkeitsgrad: 0,
+      highScore: 0
     };
 
+    service.logIn = function logIn(user, password) {
+      ConfigService.login.get({user: user, password: password}).then(
+        function(result){
+          user.benutzerId = result.data.benutzerid;
+          user.benutzerName = result.data.benutzername;
+          user.fettigkeitsgrad = result.data.fettigkeitsgrad;
+          user.highScore = result.data.highscore;
+
+        }
+      );
+    };
 
     return service;
   }]);
