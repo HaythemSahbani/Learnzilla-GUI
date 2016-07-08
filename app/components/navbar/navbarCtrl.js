fc2d83413cd87081130890c8ae6e23282557c11a
Haythem Sahbani <EXTERN.SAHBANI_HAYTHEM@ALLIANZ.DE>
2016-07-08 10:21:20 +0200

    zilla impl

--- app/components/navbar/navbarCtrl.js
+++ app/components/navbar/navbarCtrl.js
@@ -2,18 +2,23 @@
 
 angular.module('learnzillaApp')
 
-    .controller('navbarCtrl', [ 'ViewsService', 'Constants', 'Model',  function (ViewsService, Constants, Model) {
-      const vm = this;
-      vm.user = Model.user;
-      vm.goTologInPage = function goTologInPage() {
-        return ViewsService.setCurrentView(Constants.SIGN_UP);
-      };
-      vm.goToStoryPage = function goToStoryPage() {
-        return ViewsService.setCurrentView(Constants.STORY);
-      };
-      vm.goToContactUsPage = function goToContactUsPage() {
-        return ViewsService.setCurrentView(Constants.CONTACT_US);
-      };
+  .controller('navbarCtrl', ['ViewsService', 'Constants', 'Model', 'UserService',
+    function (ViewsService, Constants, Model, UserService) {
+    const vm = this;
+    vm.user = Model.user;
+    vm.goTologInPage = function goTologInPage() {
+      return ViewsService.setCurrentView(Constants.SIGN_UP);
+    };
+    vm.goToStoryPage = function goToStoryPage() {
+      return ViewsService.setCurrentView(Constants.STORY);
+    };
+    vm.goToContactUsPage = function goToContactUsPage() {
+      return ViewsService.setCurrentView(Constants.CONTACT_US);
+    };
 
+    vm.setZillaImage = function setZillaImage() {
+      return UserService.updateZilla();
     }
-    ]);
+
+  }
+  ]);
--- app/components/services/UserService.js
+++ app/components/services/UserService.js
@@ -7,7 +7,25 @@ angular.module('learnzillaApp')
     const user = Model.user;
 
     service.updateZilla = function updateZilla(){
-      
+      switch(user.fettigkeitsgrad) {
+        case 1:
+          return 'bauch_stufe1.png';
+          break;
+        case 2:
+          return 'bauch_stufe2.png';
+          break;
+        case 3:
+          return 'bauch_stufe3.png';
+          break;
+        case 4:
+          return 'bauch_stufe4.png';
+          break;
+        case 5:
+          return 'bauch_stufe5.png';
+          break;
+        default:
+        return 'assets/img/mund_zu.png'
+      }
     };
 
     return service;
