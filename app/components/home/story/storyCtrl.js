db74a6196e8bb3aa104798ad0e043618737e1b12
Haythem Sahbani <EXTERN.SAHBANI_HAYTHEM@ALLIANZ.DE>
2016-07-08 10:30:32 +0200

    zilla impl

--- app/components/home/story/storyCtrl.js
+++ app/components/home/story/storyCtrl.js
@@ -2,9 +2,12 @@
 
 angular.module('learnzillaApp')
 
-  .controller('storyCtrl', ['ViewsService', 'Constants',
-    function (ViewsService, Constants) {
+  .controller('storyCtrl', ['ViewsService', 'Constants', 'Model',
+    function (ViewsService, Constants, Model) {
       const vm = this;
+/*      vm.update = function(){
+       return  Model.user.fettigkeitsgrad++;
+      };*/
       vm.setNextView = function setNextView() {
         return ViewsService.setCurrentView(Constants.GAME_MODE);
       }
--- app/components/services/UserService.js
+++ app/components/services/UserService.js
@@ -9,19 +9,19 @@ angular.module('learnzillaApp')
     service.updateZilla = function updateZilla(){
       switch(user.fettigkeitsgrad) {
         case 1:
-          return 'bauch_stufe1.png';
+          return 'assets/img/bauch_stufe1.png';
           break;
         case 2:
-          return 'bauch_stufe2.png';
+          return 'assets/img/bauch_stufe2.png';
           break;
         case 3:
-          return 'bauch_stufe3.png';
+          return 'assets/img/bauch_stufe3.png';
           break;
         case 4:
-          return 'bauch_stufe4.png';
+          return 'assets/img/bauch_stufe4.png';
           break;
         case 5:
-          return 'bauch_stufe5.png';
+          return 'assets/img/bauch_stufe5.png';
           break;
         default:
         return 'assets/img/mund_zu.png'
